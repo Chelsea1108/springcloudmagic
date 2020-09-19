3da1eef6208bd9636db84d8a33c0bd7953083366
Chelsea1108 <xuke1108@163.com>
2020-09-19 10:25:10 +0800

    Initial commit

---
+++ src/main/java/Test.java
@@ -0,0 +1,17 @@
+public class Test {
+    public static void main(String[] args) {
+        int [] arr = {1,-2,3,5,-2,6,-1};
+
+    }
+
+    public int test (int [] arr){
+        for(int i = 0; i < arr.length;i++){
+            for(int j = i+1; j<arr.length;j++){
+                int max = arr[i];
+
+            }
+
+        }
+        return 0;
+    }
+}
---
+++ src/main/java/com/magic/SpringCloudMagicApplication.java
@@ -0,0 +1,11 @@
+package com.magic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudMagicApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudMagicApplication.class,args);
+    }
+}
